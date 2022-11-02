5c76048e06ee5d85fa5c9af07a088c0e1bb6727e
Daniel Salgado <danielsalgado.rom@gmail.com>
2022-11-01 18:27:17 -0600

    Project Finished

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Download, Button, Features, SectionWrapper } from "./components";
+import styles from "./styles/Global";
 import assets from "./assets";
 
 const App = () => {
@@ -18,6 +19,29 @@ const App = () => {
         mockupImg={assets.homeCards}
         reverse
       />
+
+      <Features />
+
+      <SectionWrapper
+        title="Deployment"
+        description="ProNef is built using Expo which runs natively on all users devices. You can easily get your app into people's hands."
+        mockupImg={assets.feature}
+        reverse
+      />
+      <SectionWrapper
+        title="Creative way to showcase the store"
+        description="The app contains two screes. The first screen lists NFTs while the second one shows the details of a specific NFT."
+        mockupImg={assets.mockup}
+        banner="banner02"
+      />
+
+      <Download />
+
+      <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
+        <p className={`${styles.pText} ${styles.whiteText}`}>
+          Made by <span className="font-bold">Daniel Salgado</span>
+        </p>
+      </div>
     </div>
   );
 };
--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+
+import assets from "../assets";
+import styles from "../styles/Global";
+
+const FeatureCard = ({ iconUrl, iconText }) => (
+  <div className={styles.featureCard}>
+    <img src={iconUrl} alt="icon" className={styles.featureImg} />
+    <p className="font-bold">{iconText}</p>
+  </div>
+);
 
 const Features = () => {
   return (
-    <div>
-      Features
-    </div>
-  )
-}
+    <div className={`${styles.section} ${styles.bgPrimary} banner03`}>
+      <div className={`${styles.subSection} flex-col text-center`}>
+        <div>
+          <h1 className={`${styles.h1Text} ${styles.whiteText}`}>
+            Technologies
+          </h1>
+          <p className={`${styles.pText} ${styles.whiteText}`}>
+            ProNef has been developed using a cross-platform technology, React
+            Native.
+          </p>
+        </div>
 
-export default Features
+        <div className={`${styles.flexWrap}`}>
+          <FeatureCard iconUrl={assets.react} iconText="React Native" />
+          <FeatureCard iconUrl={assets.javascript} iconText="JavaScript" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Features;
